856a46da07e5644008b0d8d0f32710671c4deb56
kzuben <80618481+kzuben@users.noreply.github.com>
2024-09-21 11:35:36 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -4,14 +4,13 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
 
-console.log(chosenImage);
 const bgImage = document.createElement("img");
-bgImage.src = `../images/${chosenImage}`;
+bgImage.src = `./images/${chosenImage}`;
 
 //document.body.appendChild(bgImage)
 
 const bg = document.querySelector(".page-wrapper");
-bg.style.background = "url(" + `../images/${chosenImage}` + ") no-repeat center";
+bg.style.background = "url(" + `./images/${chosenImage}` + ") no-repeat center";
 bg.style.backgroundSize = "cover"
 console.log()
 
